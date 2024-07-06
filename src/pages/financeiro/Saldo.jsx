09bf3177ed364b3add7f742806f9d26f36a0359d
RenanZanette <141188417+RenanZanette@users.noreply.github.com>
2024-07-06 15:50:04 -0300

    Saldo Arrumado

--- src/pages/financeiro/Saldo.jsx
+++ src/pages/financeiro/Saldo.jsx
@@ -49,34 +49,6 @@ const Saldo = () => {
               </div>
             </Card.Body>
           </Card>
-
-const Saldo = () => {
-  const [saldo, setSaldo] = useState(null);
-
-  useEffect(() => {
-    fetchSaldo();
-  }, []);
-
-  const fetchSaldo = async () => {
-    try {
-      const response = await axios.get('http://localhost:8080/api/entradas');
-      setSaldo(response.data);
-    } catch (error) {
-      console.error('Erro ao buscar o saldo:', error);
-    }
-  };
-
-  return (
-    <Container>
-      <Row className="vh-100 d-flex justify-content-center align-items-center">
-      <Col md={10} lg={8} xs={12}>
-          <div className="border border-3 border-primary"></div>
-          <Card className="shadow">
-              <h3 className="fw-bold mb-2 text-uppercase">Saldo</h3>
-              {/*{saldo.map((saldo) => (saldo.valor))}*/}
-                        
-           </Card>
-
         </Col>
       </Row>
     </Container>
